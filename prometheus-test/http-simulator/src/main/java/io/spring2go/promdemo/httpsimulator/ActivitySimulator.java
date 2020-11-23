dc142386489e434c98e63d6f98d1ccdfa5561bb2
sunjian <jian.sun.ms@faw-vw.com>
2020-11-23 13:20:13 +0800

    fix：修改模拟器

--- prometheus-test/http-simulator/src/main/java/io/spring2go/promdemo/httpsimulator/ActivitySimulator.java
+++ prometheus-test/http-simulator/src/main/java/io/spring2go/promdemo/httpsimulator/ActivitySimulator.java
@@ -15,12 +15,19 @@ public class ActivitySimulator implements Runnable {
 
     private volatile boolean shutdown = false;
 
+
+    /**
+     * 产生用于计数的数据
+     */
     private final Counter httpRequestsTotal = Counter.build()
         .name("http_requests_total")
         .help("Total number of http requests by response status code")
         .labelNames("endpoint", "status")
         .register();
 
+    /**
+     * 产生用于画直方图的数据
+     */
     private final Histogram httpRequestDurationMs = Histogram.build()
         .name("http_request_duration_milliseconds")
         .help("Http request latency histogram")
@@ -50,6 +57,12 @@ public class ActivitySimulator implements Runnable {
         this.opts = opts;
     }
 
+    /**
+     * 设置模式
+     *
+     * @param mode
+     * @return
+     */
     public boolean setSpikeMode(String mode) {
         boolean result = true;
         switch (mode) {
@@ -72,6 +85,11 @@ public class ActivitySimulator implements Runnable {
         return result;
     }
 
+    /**
+     * 设置错误率
+     *
+     * @param rate
+     */
     public void setErrorRate(int rate) {
         if (rate > 100) {
             rate = 100;
@@ -96,17 +114,19 @@ public class ActivitySimulator implements Runnable {
             requestRate *= (5 + this.rand.nextInt(10));
         }
 
+        //   (请求率,70)  获得波动后数值
         int nbRequests = this.giveWithUncertainty(requestRate, this.opts.getRequestRateUncertainty());
         for (int i = 0; i < nbRequests; i++) {
             String statusCode = this.giveStatusCode();
             String endpoint = this.giveEndpoint();
             this.httpRequestsTotal.labels(endpoint, statusCode).inc();
-            //
+
             int latency = this.giveLatency(statusCode);
             if (this.spikeMode) {
                 latency *= 2;
             }
             this.httpRequestDurationMs.labels(endpoint, statusCode).observe(latency);
+
         }
     }
 
@@ -141,11 +161,23 @@ public class ActivitySimulator implements Runnable {
         return this.spikeMode;
     }
 
+    /**
+     * 获得波动值，默认   n  + (  -7n ~ 7n  )
+     *
+     * @param n
+     * @param u
+     * @return
+     */
     public int giveWithUncertainty(int n, int u) {
         int delta = this.rand.nextInt(n * u / 50) - (n * u / 100);
         return n + delta;
     }
 
+    /**
+     * 获取错误率，默认1%
+     *
+     * @return
+     */
     public String giveStatusCode() {
         if (this.rand.nextInt(100) < this.opts.getErrorRate()) {
             return "500";
@@ -154,34 +186,51 @@ public class ActivitySimulator implements Runnable {
         }
     }
 
+    /**
+     * 获取接口路径，列表中随机取
+     *
+     * @return
+     */
     public String giveEndpoint() {
         int n = this.rand.nextInt(this.opts.getEndopints().length);
         return this.opts.getEndopints()[n];
     }
 
+    /**
+     * 模拟延迟
+     *
+     * @param statusCode
+     * @return
+     */
     public int giveLatency(String statusCode) {
+        //如果返回错误，返回5-55
         if (!"200".equals(statusCode)) {
             return 5 + this.rand.nextInt(50);
         }
 
+        //随机0-99
         int p = this.rand.nextInt(100);
 
         if (p < 50) {
+            // (10~25 ， 70)波动数值
             return this.giveWithUncertainty(
                 this.opts.getLatencyMin() + this.rand.nextInt(this.opts.getLatencyP50() - this.opts.getLatencyMin()),
                 this.opts.getLatencyUncertainty());
         }
         if (p < 90) {
+            // (25~150 ， 70)波动数值
             return this.giveWithUncertainty(
                 this.opts.getLatencyP50() + this.rand.nextInt(this.opts.getLatencyP90() - this.opts.getLatencyP50()),
                 this.opts.getLatencyUncertainty());
         }
         if (p < 99) {
+            // (150~750 ， 70)波动数值
             return this.giveWithUncertainty(
                 this.opts.getLatencyP90() + this.rand.nextInt(this.opts.getLatencyP99() - this.opts.getLatencyP90()),
                 this.opts.getLatencyUncertainty());
         }
 
+        // (750-10000 ， 70)波动数值
         return this.giveWithUncertainty(
             this.opts.getLatencyP99() + this.rand.nextInt(this.opts.getLatencyMax() - this.opts.getLatencyP99()),
             this.opts.getLatencyUncertainty());
